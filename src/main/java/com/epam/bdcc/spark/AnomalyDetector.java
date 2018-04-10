e9a67a0a9b2271d108144d91e29e7d32bc5020a9
Efim Poberezkin <efim@poberezkin.ru>
2018-04-10 15:14:01 +0400

    Implemented AnomalyDetector pipeline

--- src/main/java/com/epam/bdcc/spark/AnomalyDetector.java
+++ src/main/java/com/epam/bdcc/spark/AnomalyDetector.java
@@ -3,13 +3,23 @@ package com.epam.bdcc.spark;
 import com.epam.bdcc.htm.HTMNetwork;
 import com.epam.bdcc.htm.MonitoringRecord;
 import com.epam.bdcc.htm.ResultState;
+import com.epam.bdcc.kafka.KafkaHelper;
 import com.epam.bdcc.utils.GlobalConstants;
 import com.epam.bdcc.utils.PropertiesLoader;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.Optional;
+import org.apache.spark.api.java.function.Function0;
 import org.apache.spark.api.java.function.Function3;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.State;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 
@@ -38,9 +48,34 @@ public class AnomalyDetector implements GlobalConstants {
             final Duration batchDuration = Duration.apply(Long.parseLong(applicationProperties.getProperty(SPARK_BATCH_DURATION_CONFIG)));
             final Duration checkpointInterval = Duration.apply(Long.parseLong(applicationProperties.getProperty(SPARK_CHECKPOINT_INTERVAL_CONFIG)));
 
-            JavaStreamingContext jssc = null;
-            //TODO : Create your pipeline here!!!
+            Function0<JavaStreamingContext> createContext = () -> {
+                SparkConf conf = new SparkConf().setMaster("local[2]").setAppName(appName);
 
+                JavaStreamingContext jssc = new JavaStreamingContext(conf, batchDuration);
+                jssc.checkpoint(checkpointDir);
+
+                JavaInputDStream<ConsumerRecord<String, MonitoringRecord>> stream =
+                        KafkaUtils.createDirectStream(
+                                jssc,
+                                LocationStrategies.PreferConsistent(),
+                                KafkaHelper.createConsumerStrategy(rawTopicName));
+
+                stream.map(record -> mappingFunc);
+
+                stream.foreachRDD(rdd ->
+                        rdd.foreachPartition(partition -> {
+                            Producer<String, MonitoringRecord> producer = KafkaHelper.createProducer();
+                            partition.forEachRemaining(record -> {
+                                ProducerRecord<String, MonitoringRecord> message
+                                        = new ProducerRecord<>(enrichedTopicName, record.key(), record.value());
+                                producer.send(message);
+                            });
+                        }));
+
+                return jssc;
+            };
+
+            JavaStreamingContext jssc = JavaStreamingContext.getOrCreate(checkpointDir, createContext);
             jssc.start();
             jssc.awaitTermination();
         }
